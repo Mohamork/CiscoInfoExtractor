abfce6e7530b20c7a6ea66fe1eb3e673701229c4
Mohamork <k.m19981102@gmail.com>
2026-04-07 19:32:38 +0200

    Refactor ciscoinfoextractor.py for readability

--- ciscoinfoextractor.py
+++ ciscoinfoextractor.py
@@ -2,6 +2,7 @@ import re
 
 filename = "show_version.txt"
 with open (filename, "r") as f:
+    
     show_version = f.read()
 
 serial_pattern = r"^cisco\s(?P<serial_number>\S*\s\S*)"
@@ -10,10 +11,14 @@ model_pattern = r"^Processor\s.* ID\s(?P<model_number>\S*)$"
 
 match = re.search(serial_pattern,show_version, re.M)
 if match:
+    
     serial = match.group("serial_number") 
 match = re.search(model_pattern,show_version,re.M)
+
 if match: 
+    
     model = match.group("model_number")
+    
 print()    
 print(f"Serial number: {serial}")
 print()
